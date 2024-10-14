f55b824af3ab9781c0a235de4911a9b3e7da0ca6
wjacobw <williamjacob0910@gmail.com>
2024-10-14 11:42:46 +0800

    hide secrets

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -7,6 +7,7 @@ import { UsersModule } from '../users/users.module';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { GoogleStrategy } from './strategies/google.strategy';
 import { UsersService } from 'src/users/users.service';
+import process from 'process';
 
 @Module({
   imports: [
